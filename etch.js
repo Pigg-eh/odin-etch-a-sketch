b6d732923362714dc5ebde00d2e830e520550594
Hunter <ep.hunter23@gmail.com>
2023-08-23 18:03:15 -0700

    Add 16 divs to container div

--- etch.js
+++ etch.js
@@ -1,4 +1,26 @@
+// function applyStyles () {
+//     document.getElementsByClassName('littleDiv').style.color = 'blue'
+// }
 
-function grid (){
-    //4 by 4 grid how do?
+let squArray = []
+makeDiv()
+
+function makeDiv (){
+    for (i=0; i<16; i++){
+        squArray.push('div'+i)
+    }
+}
+
+insertDiv()
+
+function insertDiv(){
+    let divContainer= document.querySelector('div.container')
+    for (i=0; i<16; i++){
+        makeDiv[i]= document.createElement('div')
+        divContainer.appendChild(makeDiv[i])
+    }
+}
+
+function applyStyles(){
+
 }

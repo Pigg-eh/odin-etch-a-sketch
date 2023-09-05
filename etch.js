74d222ae3953d6d765923c84a4312163e5e125ad
Hunter <ep.hunter23@gmail.com>
2023-09-04 17:14:12 -0700

    Add display functions and eventListener for input
    Not a working iteration of the eventListener

--- etch.js
+++ etch.js
@@ -1,8 +1,9 @@
 let squArray = []
+let gridSize = 16
 
 makeDiv()
-function makeDiv (){
-    for (i=0; i<256; i++){
+function makeDiv (x=256){
+    for (i=0; i<x; i++){
         squArray.push('div'+i)
     }
 }
@@ -17,22 +18,36 @@ function createResizeSlider() {
     slider.setAttribute('type', 'range')
     slider.setAttribute('min', '1')
     slider.setAttribute('max', '64')
-    slider.setAttribute('value', '16')
+    slider.setAttribute('value', gridSize)
 
 
     container.classList.add('slideContainer')
     slider.classList.add('slider')
 }
 
+createSliderDisplay()
+function createSliderDisplay() {
+    let container = document.querySelector('div.slideContainer')
+    let sliderDisplay = document.createElement('div')
+    container.appendChild(sliderDisplay)
+    sliderDisplay.classList.add('display') 
+    sliderDisplay.textContent = 'test'
+} 
 
+displaySliderValue ()
+function displaySliderValue(value){
+    //let currentValue = document.querySelector('input.slider')
+    let sliderDisplay = document.querySelector('div.display')
+    sliderDisplay.textContent = `Grid ${value}`
+}
 
-function doMathMultiply(x) {
-	x**2
-}// number of divs to make
+function doMathMultiply(x /*passed from slider */) {
+	return x**2
+}// number of divs to make 
 
-function doMathDivision(x){
-	1000/x
-}//size of individual divs
+function doMathDivision(x/*also passed from slider*/){
+	return 1000/x
+}//size of individual divs (change div.pixel attribute)
 
 insertDiv()
 function insertDiv(x=256) {
@@ -43,16 +58,29 @@ function insertDiv(x=256) {
     }
 } 
 
-//resize()
-
-addClass()
-function addClass() {
-    let pixels = document.querySelectorAll('div>div')
+addGridClass()
+function addGridClass() {
+    let pixels = document.querySelectorAll('div.container>div')
 
     pixels.forEach((pixel) => {
         pixel.classList.add('pixel')
     })
 }
+
+resizeGrid()
+function resizeGrid() {
+    let sliderValue = document.querySelector('input')
+
+    
+        sliderValue.addEventListener('range', (e) => {
+            gridSize = e.target.value
+        })
+    
+
+    displaySliderValue(gridSize)
+}
+
+
 
 hoverOver()
 function hoverOver() {
@@ -68,6 +96,6 @@ function hoverOver() {
 
 
 
-//ResizeGrid()
+
 
     

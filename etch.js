1a069ee87f8d2b61d5d3793c8f02b900e45ab009
Hunter <ep.hunter23@gmail.com>
2023-09-09 13:32:09 -0700

    div.Pixel arrange and size properly
    Need to fix the button display

--- etch.js
+++ etch.js
@@ -1,10 +1,11 @@
-let squArray = [] 
-let product = x => x**2// number of divs to make 
-let quotient = x=> 1000/x  
+let squArray = []
+let sliderValue = 16
+let findProduct = (x) => x**2;// number of divs to make 
+let findQuotient = (x)=>1000/x;  
 
-//check pixel.div it is not even changing the DOM
-//size of individual divs (change div.pixel attribute)
-//parameters passed from slider
+console.log(findProduct(5))
+//function displaySliderValue() not changing the button
+
 
 makeDiv(256)
 function makeDiv (x){
@@ -47,24 +48,26 @@ function styleDiv (x= 62.5){
 function clearDiv (){ //remove div.pixel temporarily
     let pixels = document.querySelectorAll('.pixel')
 
+
+
     pixels.forEach((pixel) => {
         pixel.remove();
     })
 
     squArray = []
 
-    doMaths(product, quotient) //functions returning null
+    doMaths(findProduct(sliderValue), findQuotient(sliderValue)) //functions returning NaN
 
 }
 
 
 
-function doMaths (product, quotient){ //immediately call the functions to add div again
+function doMaths (x, y){ //immediately call the functions to add div again
 
-    makeDiv(product)
-    insertDiv(product)
-    styleDiv(quotient) //may be the issue
-}
+    makeDiv(x)
+    insertDiv(x)
+    styleDiv(y)
+}//values are coming out as NaN here
 
 createResizeSlider()
 function createResizeSlider() {
@@ -94,7 +97,9 @@ function createResizeButton() {
 displaySliderValue (16)
 function displaySliderValue(value){
     let sliderDisplay = document.querySelector('button.display')
+    
     sliderDisplay.textContent = `Grid ${value}`
+    console.log(`VALUEPARAM: ${value}`)
 }
 
 addClickListener()
@@ -103,7 +108,8 @@ function addClickListener() {
 
 
     btn.addEventListener ('click',  () => {   
-        clearDiv()   
+        clearDiv(sliderValue) 
+          
     })
 }
 
@@ -111,17 +117,19 @@ function addClickListener() {
 
 
 
-resizeGrid()
+resizeGrid(sliderValue)
 function resizeGrid() {
-    let sliderValue = document.querySelector('input.slider')
+    let slidersValue = document.querySelector('input.slider')
 
     
-        sliderValue.addEventListener('change', (e) => {
-            let displayValue = e.target.value;
-            displaySliderValue(displayValue)
-            product (displayValue)
-            quotient (displayValue)
+        slidersValue.addEventListener('change', (e) => {
+            sliderValue = e.target.value;
+            console.log(`HERESTUPIDsliderValue@resizeGrid(): ${sliderValue}`)
         }) 
+        displaySliderValue(sliderValue)
+        findProduct (sliderValue)
+        console.log(`displayVal@resizeGrid(): ${sliderValue}`)
+        findQuotient (sliderValue)
 }
 
 
